108b1fe8e57709ecda0da36fe56a1a172524e7b5
WingChhun <capt.wing.chhun@gmail.com>
2018-07-07 15:01:02 -0700

    Copied Menu from Menu in Electron API, and uncommented Chromium DEveloper tools

--- src/main.js
+++ src/main.js
@@ -1,49 +1,193 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {Menu, app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600})
+    const template = [
+        {
+            label: "File",
+            submenu: [
+                {
+                    label: "Open File"
+                }, {
+                    label: "Open Folder"
+                }
+            ]
+        }, {
+            label: 'Edit',
+            submenu: [
+                {
+                    role: 'undo'
+                }, {
+                    role: 'redo'
+                }, {
+                    type: 'separator'
+                }, {
+                    role: 'cut'
+                }, {
+                    role: 'copy'
+                }, {
+                    role: 'paste'
+                }, {
+                    role: 'pasteandmatchstyle'
+                }, {
+                    role: 'delete'
+                }, {
+                    role: 'selectall'
+                }
+            ]
+        }, {
+            label: 'View',
+            submenu: [
+                {
+                    role: 'reload'
+                }, {
+                    role: 'forcereload'
+                }, {
+                    role: 'toggledevtools'
+                }, {
+                    type: 'separator'
+                }, {
+                    role: 'resetzoom'
+                }, {
+                    role: 'zoomin'
+                }, {
+                    role: 'zoomout'
+                }, {
+                    type: 'separator'
+                }, {
+                    role: 'togglefullscreen'
+                }
+            ]
+        }, {
+            role: 'window',
+            submenu: [
+                {
+                    role: 'minimize'
+                }, {
+                    role: 'close'
+                }
+            ]
+        }, {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click() {
+                        require('electron')
+                            .shell
+                            .openExternal('https://electronjs.org')
+                    }
+                }
+            ]
+        }
+    ];
+    //If macOS
+    if (process.platform === 'darwin') {
+        template.unshift({
+            label: app.getName(),
+            submenu: [
+                {
+                    role: 'about'
+                }, {
+                    type: 'separator'
+                }, {
+                    role: 'services',
+                    submenu: []
+                }, {
+                    type: 'separator'
+                }, {
+                    role: 'hide'
+                }, {
+                    role: 'hideothers'
+                }, {
+                    role: 'unhide'
+                }, {
+                    type: 'separator'
+                }, {
+                    role: 'quit'
+                }
+            ]
+        })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000')
+        // Edit menu
+        template[2]
+            .submenu
+            .push({
+                type: 'separator'
+            }, {
+                label: 'Speech',
+                submenu: [
+                    {
+                        role: 'startspeaking'
+                    }, {
+                        role: 'stopspeaking'
+                    }
+                ]
+            })
 
-  // Open the DevTools.
-   mainWindow.webContents.openDevTools()
+        // Window menu
+        template[3].submenu = [
+            {
+                role: 'close'
+            }, {
+                role: 'minimize'
+            }, {
+                role: 'zoom'
+            }, {
+                type: 'separator'
+            }, {
+                role: 'front'
+            }
+        ]
+    }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    const menu = Menu.buildFromTemplate(template);
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:3000')
+
+    Menu.setApplicationMenu(menu);
+
+    // Open the DevTools.
+    mainWindow
+        .webContents
+        .openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows in an array if
+        // your app supports multi windows, this is the time when you should delete the
+        // corresponding element.
+        mainWindow = null
+    })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// This method will be called when Electron has finished initialization and is
+// ready to create browser windows. Some APIs can only be used after this event
+// occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar to stay active until
+    // the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the dock icon is
+    // clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
