eb929487a4a2569b51556826660153e004bf796c
James Chhun <jchhun@circadence.com>
2018-07-11 19:32:45 -0700

    Finish lessons 11, install react devtools and packages

--- src/App.js
+++ src/App.js
@@ -1,98 +1,107 @@
-import React, {Component} from 'react';
-import Markdown from 'markdown-to-jsx';
-import AceEditor from 'react-ace';
-import brace from 'brace';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import Markdown from "markdown-to-jsx";
+import AceEditor from "react-ace";
+import brace from "brace";
+import styled from "styled-components";
 
-import 'brace/mode/markdown';
-import 'brace/theme/dracula';
-import './App.css';
+import "brace/mode/markdown";
+import "brace/theme/dracula";
+import "./App.css";
 
-const {ipcRenderer} = window.require('electron');
+const { ipcRenderer } = window.require("electron");
 
 class App extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
+    this.state = {
+      loadedFile: ""
+    };
 
-        this.state = {
-            loadedFile: ''
-        };
+    ipcRenderer.on("new-file", (event, fileContent) => {
+      this.setState({ loadedFile: fileContent });
+    });
 
-        ipcRenderer.on('new-file', (event, fileContent) => {
-            this.setState({loadedFile: fileContent});
-        });
-    }
+    ipcRenderer.on("new-dir", (event, filePaths, dir) => {
+      this.setState({
+        directory: dir
+      });
+    });
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <Header>
-                    Journal
-                </Header>
-                <Split>
-                    <CodeWindow>
-                        <AceEditor
-                            mode="markdown"
-                            theme="dracula"
-                            onChange=
-                            {(newContent)=>{ this.setState({loadedFile:newContent}) }}
-                            name="markdown_editor"
-                            value={this.state.loadedFile}/>
-                    </CodeWindow>
+  render() {
+    return (
+      <div className="App">
+        <Header>Journal</Header>
+        <Split>
+          <CodeWindow>
+            <AceEditor
+              mode="markdown"
+              theme="dracula"
+              onChange={newContent => {
+                this.setState({ loadedFile: newContent });
+              }}
+              name="markdown_editor"
+              value={this.state.loadedFile}
+            />
+          </CodeWindow>
 
-                    <RenderedWindow>
-                        <Markdown>{this.state.loadedFile}</Markdown>
-                    </RenderedWindow>
-                </Split>
-            </div>
-        );
-    }
+          <RenderedWindow>
+            <Markdown>{this.state.loadedFile}</Markdown>
+          </RenderedWindow>
+        </Split>
+      </div>
+    );
+  }
 }
 
 export default App;
 
-const Header = styled.header `
-background-color:#191324;
-color:#75717c;
-font-size:0.8rem;
-height:23px;
-text-align:center;
-position:fixed;
-box-shadow:0px 3px 3px rgba(0,0,0,0.2);
-top:0;
-left:0;
-width:100%;
-z-index:10;
--webkit-app-region:drag;
+const Header = styled.header`
+  background-color: #191324;
+  color: #75717c;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.2);
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
 `;
 
-const Split = styled.div `
-display:flex;
-height:100vh;
-
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
 `;
 
-const CodeWindow = styled.div `
-flex:1;
-padding-top:2rem;
-background-color:#191324;
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
 `;
 
-const RenderedWindow = styled.div `
-background-color:#191324;
-width:35%;
-padding:20px;
-color:#fff;
-border-left:1px solid #302b3a;
-h1,h2,h3,h4,h5,h6{
-  color:#82d8d8
-}
-h1{
-  border-bottom:solid 3px #e54b4b;
-  padding-bottom:10px;
-}
-a{
-  color:#e54b4b
-}
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #82d8d8;
+  }
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #e54b4b;
+  }
 `;
--- src/main.js
+++ src/main.js
@@ -1,239 +1,321 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, dialog, Menu} = require('electron');
-const fs = require('fs');
+const { app, BrowserWindow, dialog, Menu } = require("electron");
+const fs = require("fs");
+const {
+  default: installExtension,
+  REACT_DEVELOPER_TOOLS
+} = require("electron-devtools-installer");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 1200, height: 800, titleBarStyle: 'hidden'});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    titleBarStyle: "hidden"
+  });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
+  // and load the index.html of the app.
+  mainWindow.loadURL("http://localhost:3000");
 
-    const template = [
+  const template = [
+    {
+      label: "File",
+      submenu: [
+        // {
+        //   label: "Open File",
+        //   accelerator: "CmdOrCtrl+O",
+        //   click() {
+        //     openFile();
+        //   }
+        // },
         {
-            label: 'File',
-            submenu: [
-                {
-                    label: 'Open File',
-                    accelerator: 'CmdOrCtrl+O',
-                    click() {
-                        openFile();
-                    }
-                }, {
-                    label: 'Open Folder'
-                }
-            ]
-        }, {
-            label: 'Edit',
-            submenu: [
-                {
-                    role: 'undo'
-                }, {
-                    role: 'redo'
-                }, {
-                    type: 'separator'
-                }, {
-                    role: 'cut'
-                }, {
-                    role: 'copy'
-                }, {
-                    role: 'paste'
-                }, {
-                    role: 'pasteandmatchstyle'
-                }, {
-                    role: 'delete'
-                }, {
-                    role: 'selectall'
-                }
-            ]
-        }, {
-            label: 'View',
-            submenu: [
-                {
-                    role: 'reload'
-                }, {
-                    role: 'forcereload'
-                }, {
-                    role: 'toggledevtools'
-                }, {
-                    type: 'separator'
-                }, {
-                    role: 'resetzoom'
-                }, {
-                    role: 'zoomin'
-                }, {
-                    role: 'zoomout'
-                }, {
-                    type: 'separator'
-                }, {
-                    role: 'togglefullscreen'
-                }
-            ]
-        }, {
-            role: 'window',
-            submenu: [
-                {
-                    role: 'minimize'
-                }, {
-                    role: 'close'
-                }
-            ]
-        }, {
-            role: 'help',
-            submenu: [
-                {
-                    label: 'Learn More',
-                    click() {
-                        require('electron')
-                            .shell
-                            .openExternal('https://electronjs.org');
-                    }
-                }
-            ]
-        }, {
-            label: 'Developer',
-            submenu: [
-                {
-                    label: 'Toggle Developer Tools',
-                    accelerator: process.platform === 'darwin'
-                        ? 'Alt+Command+I'
-                        : 'Ctrl+Shift+I',
-                    click() {
-                        mainWindow
-                            .webContents
-                            .toggleDevTools();
-                    }
-                }
-            ]
+          label: "Open Folder",
+          accelerator: "CmdOrCtrl+O",
+          click() {
+            openDir();
+          }
         }
-    ];
-
-    // If macOS
-    if (process.platform === 'darwin') {
-        template.unshift({
-            label: app.getName(),
-            submenu: [
-                {
-                    role: 'about'
-                }, {
-                    type: 'separator'
-                }, {
-                    role: 'services',
-                    submenu: []
-                }, {
-                    type: 'separator'
-                }, {
-                    role: 'hide'
-                }, {
-                    role: 'hideothers'
-                }, {
-                    role: 'unhide'
-                }, {
-                    type: 'separator'
-                }, {
-                    role: 'quit'
-                }
-            ]
-        });
-
-        // Edit menu
-        template[2]
-            .submenu
-            .push({
-                type: 'separator'
-            }, {
-                label: 'Speech',
-                submenu: [
-                    {
-                        role: 'startspeaking'
-                    }, {
-                        role: 'stopspeaking'
-                    }
-                ]
-            });
-
-        // Window menu
-        template[4].submenu = [
-            {
-                role: 'close'
-            }, {
-                role: 'minimize'
-            }, {
-                role: 'zoom'
-            }, {
-                type: 'separator'
-            }, {
-                role: 'front'
-            }
-        ];
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        {
+          role: "undo"
+        },
+        {
+          role: "redo"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "cut"
+        },
+        {
+          role: "copy"
+        },
+        {
+          role: "paste"
+        },
+        {
+          role: "pasteandmatchstyle"
+        },
+        {
+          role: "delete"
+        },
+        {
+          role: "selectall"
+        }
+      ]
+    },
+    {
+      label: "View",
+      submenu: [
+        {
+          role: "reload"
+        },
+        {
+          role: "forcereload"
+        },
+        {
+          role: "toggledevtools"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "resetzoom"
+        },
+        {
+          role: "zoomin"
+        },
+        {
+          role: "zoomout"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "togglefullscreen"
+        }
+      ]
+    },
+    {
+      role: "window",
+      submenu: [
+        {
+          role: "minimize"
+        },
+        {
+          role: "close"
+        }
+      ]
+    },
+    {
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click() {
+            require("electron").shell.openExternal("https://electronjs.org");
+          }
+        }
+      ]
+    },
+    {
+      label: "Developer",
+      submenu: [
+        {
+          label: "Toggle Developer Tools",
+          accelerator:
+            process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+          click() {
+            mainWindow.webContents.toggleDevTools();
+          }
+        }
+      ]
     }
+  ];
 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+  // If macOS
+  if (process.platform === "darwin") {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {
+          role: "about"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "services",
+          submenu: []
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "hide"
+        },
+        {
+          role: "hideothers"
+        },
+        {
+          role: "unhide"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "quit"
+        }
+      ]
+    });
 
-    // Open the DevTools. mainWindow.webContents.openDevTools() Emitted when the
-    // window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows in an array if
-        // your app supports multi windows, this is the time when you should delete the
-        // corresponding element.
-        mainWindow = null;
+    // Edit menu
+    template[2].submenu.push(
+      {
+        type: "separator"
+      },
+      {
+        label: "Speech",
+        submenu: [
+          {
+            role: "startspeaking"
+          },
+          {
+            role: "stopspeaking"
+          }
+        ]
+      }
+    );
+
+    // Window menu
+    template[4].submenu = [
+      {
+        role: "close"
+      },
+      {
+        role: "minimize"
+      },
+      {
+        role: "zoom"
+      },
+      {
+        type: "separator"
+      },
+      {
+        role: "front"
+      }
+    ];
+  }
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  // window is closed.
+  mainWindow.on("closed", function() {
+    // Dereference the window object, usually you would store windows in an array if
+    // your app supports multi windows, this is the time when you should delete the
+    // corresponding element.
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.openDevTools();
+  //install React Devtools and Redux
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then(msg => {
+      console.log("Added extensnion ", msg);
+    })
+    .catch(err => {
+      console.log("Error adding extension ", err);
     });
 }
 
 // This method will be called when Electron has finished initialization and is
 // ready to create browser windows. Some APIs can only be used after this event
 // occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar to stay active until
-    // the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("ready", () => {
+  createWindow();
 });
 
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the dock icon is
-    // clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+  // On OS X it is common for applications and their menu bar to stay active until
+  // the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function() {
+  // On OS X it's common to re-create a window in the app when the dock icon is
+  // clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here. Open
 // File
 function openFile() {
-    // Opens file dialog looking for markdown
-    const files = dialog.showOpenDialog(mainWindow, {
-        properties: ['openFile'],
-        filters: [
-            {
-                name: 'Markdown',
-                extensions: ['md', 'markdown', 'txt']
-            }
-        ]
-    });
+  // Opens file dialog looking for markdown
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ["openFile"],
+    filters: [
+      {
+        name: "Markdown",
+        extensions: ["md", "markdown", "txt"]
+      }
+    ]
+  });
 
-    // If no files
-    if (!files) 
-        return;
-    
-    const file = files[0]; // Grabs first file path in array
-    // Loads file contents via path acquired via the dialog
-    const fileContent = fs
-        .readFileSync(file)
-        .toString();
+  // If no files
+  if (!files) return;
 
-    // Send filedContent to renderer
-    mainWindow
-        .webContents
-        .send('new-file', fileContent);
+  const file = files[0]; // Grabs first file path in array
+  // Loads file contents via path acquired via the dialog
+  const fileContent = fs.readFileSync(file).toString();
+
+  // Send filedContent to renderer
+  mainWindow.webContents.send("new-file", fileContent);
 }
+
+//Opens directory
+const openDir = () => {
+  const directory = dialog.showOpenDialog(mainWindow, {
+    properties: ["openDirectory"],
+    filters: [
+      {
+        name: "Markdown",
+        extensions: ["md", "markdown", "txt"]
+      }
+    ]
+  });
+
+  if (!directory) {
+    return;
+  }
+  const dir = directory[0];
+  let filePaths = [];
+  let filteredFiles = [];
+
+  fs.readdir(directory[0], (err, files) => {
+    filteredFiles = files.filter(file => file.includes(".md"));
+    filePaths = filteredFiles.map(file => `${dir}/${file}`);
+
+    //*DEBUG
+    console.log("Files ", filteredFiles);
+    console.log("File paths ", filePaths);
+  });
+
+  //Send filePaths and directory to mainWindow
+  mainWindow.webContents.send("new-dir", filePaths, dir);
+};
